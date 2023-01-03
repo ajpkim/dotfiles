d147e639949d6b5cbbdcadaf16bb1aa2ad2176aa
Alex Kim <dev@ajpkim.com>
2023-01-03 15:46:03 -0500

    Fix prettier hooks

--- emacs/.emacs.d/config/ak-prettier-js.el
+++ emacs/.emacs.d/config/ak-prettier-js.el
@@ -1,20 +1,18 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Code formatting with PrettierJS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (provide 'ak-prettier-js)
 
+;; Cannot set the hooks in use-package declaration here for some reason...
 (use-package prettier-js
-  :ensure t
-  :hook
-  (typescript-ts-mode-hook . prettier-js-mode)
-  (tsx-ts-mode-hook . prettier-js-mode)
-  )
+  :ensure t)
 
-;; (add-hook 'typescript-ts-mode-hook #'(lambda ()
-;; 				       (enable-minor-mode
-;; 					'("\\.jsx?\\'" . prettier-js-mode))
-;; 				       (enable-minor-mode
-;; 					'("\\.tsx?\\'" . prettier-js-mode))))
+(add-hook 'tsx-ts-mode-hook 'prettier-js-mode)
+(add-hook 'typescript-ts-mode-hook 'prettier-js-mode)
 
-;; (add-hook 'tsx-ts-mode-hook #'(lambda ()
-;; 				       (enable-minor-mode
-;; 					'("\\.jsx?\\'" . prettier-js-mode))
-;; 				       (enable-minor-mode
-;; 					'("\\.tsx?\\'" . prettier-js-mode))))
+(setq prettier-js-args '("--trailing-comma" "all"
+			 "--bracket-spacing" "true"
+			 "--semi" "false"
+			 "--singleQuote" "true"
+			 ))
