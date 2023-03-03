de5e9b2a32d1fd80167153647a7653f0ddfc0605
Alex Kim <dev@ajpkim.com>
2023-03-02 22:50:51 -0500

    Add to js/ts emacs config

--- emacs/.emacs.d/config/ak-js-ts.el
+++ emacs/.emacs.d/config/ak-js-ts.el
@@ -5,6 +5,12 @@
 
 (provide 'ak-js-ts)
 
+
+(add-hook 'js-mode 'js-ts-mode)
+(add-hook 'typescript-mode-hook 'typescript-ts-mode)
+
+
 ;; Formatting with prettier
 (add-hook 'js-ts-mode-hook 'prettier-js-mode)
+(add-hook 'tsx-ts-mode-hook 'prettier-js-mode)
 (add-hook 'typescript-ts-mode-hook 'prettier-js-mode)
